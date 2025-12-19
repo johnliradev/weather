a8781b367668c35a42710501f37ed9a1cac5028c
John <johnalsoares@gmail.com>
2025-12-18 21:18:45 -0300

    Add frontend setup with React, TypeScript, and Vite. Includes Docker configuration, and initial application structure.

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className="font-geist min-h-screen flex flex-col justify-center items-center">
+      <p className="text-4xl font-bold">Weather</p>
+    </main>
+  );
+}
+
+export default App;
